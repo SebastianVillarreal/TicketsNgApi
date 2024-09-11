2105fa3e1863a4196b1f8cab194fde40c2ac1160
Debban-art <debbany.545@gmail.com>
2024-09-10 20:21:08 -0600

    Actualización TicketEstatus

--- Models/TicketEstatusModel.cs
+++ Models/TicketEstatusModel.cs
@@ -6,8 +6,8 @@ namespace reportesApi.Models
         public int Ticket_Id {get; set;}
         public string Ticket_Descripcion {get; set;}
         public string Ticket_Fecha {get; set;}
-        public int Tipo_Id {get; set;}
-        public string Tipo_Nombre {get; set;}
+        public int Tipo_Ticket_Id {get; set;}
+        public string Tipo_Ticket_Nombre {get; set;}
         public string Modulo_Nombre {get; set;}
         public string Sistema_Nombre {get; set;}
         public int Estatus_Id {get; set;}
